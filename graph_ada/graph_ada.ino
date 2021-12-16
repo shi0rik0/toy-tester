5f2979004bfdecbfcfd62428f0b6e9f9aa49e1da
MadCreeper <sunjichen21@126.com>
2021-12-16 15:54:35 +0800

    ”增加画图插值,现在可以画平滑的曲线“

--- graph_ada/graph_ada.ino
+++ graph_ada/graph_ada.ino
@@ -42,10 +42,15 @@ Adafruit_PCD8544 lcd = Adafruit_PCD8544(3,4,5,7,6);
 // and written to during SPI transfer.  Be careful sharing these pins!
 
 const uint8_t LCD_WIDTH = 84, LCD_HEIGHT = 48;
-
+/*
+float __fmap(float x, float l1, float r1, float l2, float r2){
+  return (x - l1) * (r2 - l2) / (r1 - l1) + l2;
+}
+*/
 inline void _setPixel(int x, int y, bool color){
   lcd.drawPixel(x, LCD_HEIGHT - y, color);
 }
+
 void setup()   {
   Serial.begin(9600);
   Serial.println("PCD test");
@@ -55,10 +60,10 @@ void setup()   {
   // draw a single pixel
   //for(int i = 0; i < 80; ++i)
   //lcd.drawPixel(i, 10, BLACK);
-  delay(1000);
+  delay(50);
 }
 
-void graph(uint8_t *x, uint8_t *y, int len){ // 画测试图像
+void testGraph(uint8_t *x, uint8_t *y, int len){ // 画测试图像
   int WIDTH = len;
   const int HEIGHT = LCD_HEIGHT;
   int SIZE = WIDTH * HEIGHT;
@@ -73,16 +78,59 @@ void graph(uint8_t *x, uint8_t *y, int len){ // 画测试图像
   lcd.display();
 }
 
+void graph(float *X, float *Y, float xMin, float xMax, float yMin, float yMax, int len) {  // xMin, xMax分别为x轴最小、最大刻度, y同理; len: 数据点个数
+  int px, py; // previous x and y
+  for(int i = 0; i < len; ++i){
+    int x = round(map(X[i], xMin, xMax, 0, LCD_WIDTH - 1));
+    int y = round(map(Y[i], yMin, yMax , 0, LCD_HEIGHT - 1));
+    Serial.print(x);
+    Serial.print(' ');
+    Serial.print(y);
+    Serial.print('\n');
+    _setPixel(x, y , BLACK);
+    if(i == 0){
+      ;
+    }
+    // 连线
+    
+    float incl = x == px ? 1e7 : (float)(y - py) / (float)(x - px);
+    if(incl <= 1){
+      for(int j = 1; px + j < x; ++j){
+      _setPixel(px + j, py + round(j * incl), BLACK);
+      }
+    }
+    else{
+      for(int k = 1; py + k < y; ++k){
+        _setPixel(px + round(k / incl), py + k ,BLACK);
+      }
+    }
+    delay(5);
+    px = x;
+    py = y;
+  }
+  lcd.display();
+}
 void loop() {
-  const int len = LCD_WIDTH;
-  uint8_t x[len], y[len];
+  const int len = 11;
+  float x[len], y[len];
   for(int i = 0; i < len; i++){
     x[i] = i;
-    y[i] = (i)/2;
+    y[i] = 2 * i;
   }
-  graph(x,y,len);
-  delay(10);
-  
+  graph(x,y, 0, 40, 0, 27, len);
+  for(int i = 0; i < len; i++){
+    x[i] = i;
+    y[i] = 1 * i;
+  }
+  graph(x,y, 0, 40, 0, 27, len);
+  for(int i = 0; i < len; i++){
+    x[i] = i;
+    y[i] = i * i / 4;
+  }
+  graph(x,y, 0, 40, 0, 27, len);
+  delay(1000);
+
+  /*
   for(int i = 0; i < len; i++){
     x[i] = i;
     y[i] = (i)/4;
@@ -96,6 +144,6 @@ void loop() {
   }
   graph(x,y,len);
   delay(10);
-  
+  */
   //lcd.clearDisplay();
 }
